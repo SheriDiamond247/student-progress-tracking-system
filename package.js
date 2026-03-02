9cfdaf46da8e40afdea514b0df7d1ed0f4a57023
SheriDiamond247 <sherifatbalogun442@gmail.com>
2026-03-02 18:47:04 +0100

    Change main entry point from index.ts to index.js

--- package.js
+++ package.js
@@ -1,17 +1,13 @@
 {
   "name": "final-year-backend",
   "version": "1.0.0",
-  "main": "index.ts",
+  "main": "index.js",
+  "license": "MIT",
   "scripts": {
-    "start": "npx ts-node index.ts"
+    "start": "node index.js"
   },
   "dependencies": {
     "express": "^4.18.2",
     "pg": "^8.11.1"
-  },
-  "devDependencies": {
-    "typescript": "^5.2.2",
-    "ts-node": "^10.9.1",
-    "@types/node": "^20.6.1"
   }
 }
